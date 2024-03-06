e6737199d3c2a9defe624b350ba4f45305d64d8e
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-06 18:20:29 -0500

    style: add custom styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				"back":"#fafafa",
+				"back-2":"#f3f4f2",
+				// "brand":"#3e87ad"
+				"brand":"#33a7e0",
+				"txt":"#0c3445"
+			},
+			fontFamily:{
+				poppins:["Poppins",...defaultTheme.fontFamily.sans]
+			}
+		},
 	},
 	plugins: [],
 }
