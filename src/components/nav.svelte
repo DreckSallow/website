1e4ccd3381cb0ace30138f9052b6cb4c233d0605
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-06 09:04:37 -0500

    feat: add nav and section pages

---
+++ src/components/nav.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { page } from "../store";
+  import { UserIcon, BriefcaseIcon, BulbIcon } from "./icons";
+</script>
+
+<nav
+  class="m-auto rounded-full bg-blue-300 fixed bottom-2 h-[40px] left-1/2 -translate-x-1/2"
+>
+  <div
+    class="w-full h-full py-2 px-3 flex flex-row gap-4 text-white"
+    role="list"
+  >
+    <button class="cursor-pointer" on:click={() => page.set("info")}>
+      <UserIcon className="w-6 h-6 fill-blue-800" />
+    </button>
+    <button class="cursor-pointer" on:click={() => page.set("jobs")}>
+      <BriefcaseIcon className="w-6 h-6 fill-blue-800" />
+    </button>
+    <button class="cursor-pointer" on:click={() => page.set("projects")}>
+      <BulbIcon className="w-6 h-6 fill-blue-800" />
+    </button>
+  </div>
+</nav>
---
+++ src/components/sections.svelte
@@ -0,0 +1,11 @@
+<script>
+  import AboutSection from "./views/about.svelte";
+  import JobsSection from "./views/jobs.svelte";
+  import { page } from "../store";
+</script>
+
+{#if $page == "info"}
+  <AboutSection />
+{:else if $page == "jobs"}
+  <JobsSection />
+{/if}
