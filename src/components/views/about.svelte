59dcae586b9f28ff8d21349695654c9a37df1269
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-07 12:53:02 -0500

    style: change some styles

--- src/components/views/about.svelte
+++ src/components/views/about.svelte
@@ -16,17 +16,17 @@
   class="flex justify-between items-center gap-12 lg:gap-6 m-auto mx-6 flex-col lg:flex-row"
 >
   <div class="lg:w-1/2">
-    <h5 class="text-neutral-400 font-medium text-sm lg:text-base mb-1">
+    <h5 class="text-brand font-medium text-xs lg:text-sm mb-1">
       WELCOME TO MY WORLD
     </h5>
-    <h1 class="text-4xl lg:text-6xl font-bold mb-5">Hi, I'm Dikson Aranda</h1>
+    <h1 class="text-4xl lg:text-6xl font-bold mb-4">Hi, I'm Dikson Aranda</h1>
     <p class="text-base lg:text-lg text-justify lg:text-start">
       I'm frontend developer, passionate about technology, and focused on
       building better projects. Always for ways to expand my horizons and
       challenge myself.
     </p>
     <div class="mt-4">
-      <h5 class="mb-2 text-neutral-400 font-medium">SKILLS</h5>
+      <h5 class="mb-2 text-brand font-medium">SKILLS</h5>
       <ul class="flex gap-3 flex-wrap">
         {#each SKILLS as sk}
           <li
