cd4624bafe14cf760841025f710da5f62377d619
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-07 12:53:32 -0500

    style: add tailwind config for web

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,15 +5,24 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			colors:{
-				"back":"#fafafa",
-				"back-2":"#f3f4f2",
+			colors: {
+				"back": "#fafafa",
+				"back-2": "#f3f4f2",
 				// "brand":"#3e87ad"
-				"brand":"#33a7e0",
-				"txt":"#0c3445"
+				"brand": "#33a7e0",
+				"txt": "#0c3445"
 			},
-			fontFamily:{
-				poppins:["Poppins",...defaultTheme.fontFamily.sans]
+			fontFamily: {
+				poppins: ["Poppins", ...defaultTheme.fontFamily.sans]
+			},
+			keyframes: {
+				pager: {
+					"0%, 100%": { height: "0%" },
+					"50%": { height: "100%" }
+				}
+			},
+			animation: {
+				pager: "pager 2.5s ease-in-out"
 			}
 		},
 	},
