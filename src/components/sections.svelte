5b1989d63560d6581fa1c771d76d7c58cc1011cd
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-07 12:53:42 -0500

    style: add change page animation

--- src/components/sections.svelte
+++ src/components/sections.svelte
@@ -2,10 +2,28 @@
   import AboutSection from "./views/about.svelte";
   import JobsSection from "./views/jobs.svelte";
   import { page } from "../store";
+
+  let applyClass = false;
+  let currentPage = $page;
+  page.subscribe((pg) => {
+    if (currentPage == $page) return;
+    applyClass = true;
+    setTimeout(() => {
+      currentPage = pg;
+    }, 1300);
+    setTimeout(() => {
+      applyClass = false;
+    }, 2600);
+  });
 </script>
 
-{#if $page == "info"}
+{#if currentPage == "info"}
   <AboutSection />
-{:else if $page == "jobs"}
+{:else if currentPage == "jobs"}
   <JobsSection />
 {/if}
+
+<div
+  class="fixed bottom-0 left-0 w-screen bg-txt"
+  class:animate-pager={applyClass}
+/>
