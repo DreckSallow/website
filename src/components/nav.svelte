c3fd821384c0a7aba4da52f49c88d7c877065cbe
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-07 11:35:26 -0500

    feat: inetegrate keyboard navigation

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,23 +1,48 @@
-<script>
-  import { page } from "../store";
-  import { UserIcon, BriefcaseIcon, BulbIcon } from "./icons";
+<script lang="ts">
+  import { onMount } from "svelte";
+  // import { UserIcon, BriefcaseIcon, BulbIcon } from "./icons";
+  import { page, type PageValue } from "../store";
+  let currentI = 0;
+  const pageDirections: PageValue[] = ["info", "jobs", "projects"];
+  onMount(() => {
+    document.addEventListener("keydown", (e) => {
+      if (!e.ctrlKey) return;
+      const getI = (i: number) => {
+        if (i < 0) return 0;
+        if (i >= pageDirections.length - 1) return pageDirections.length - 1;
+        return i;
+      };
+      const key = e.key;
+      if (key == "ArrowRight") {
+        currentI = getI(currentI + 1);
+        page.set(pageDirections[currentI]);
+      }
+      if (key == "ArrowLeft") {
+        currentI = getI(currentI - 1);
+        page.set(pageDirections[currentI]);
+      }
+    });
+  });
 </script>
 
 <nav
-  class="m-auto rounded-full bg-blue-300 fixed bottom-2 h-[40px] left-1/2 -translate-x-1/2"
+  class="m-auto rounded-full bg-brand/20 fixed bottom-2 left-1/2 -translate-x-1/2 backdrop-blur-lg"
 >
   <div
-    class="w-full h-full py-2 px-3 flex flex-row gap-4 text-white"
+    class="w-full h-full py-2 px-3 flex flex-row gap-4 text-txt font-medium text-sm"
     role="list"
   >
     <button class="cursor-pointer" on:click={() => page.set("info")}>
-      <UserIcon className="w-6 h-6 fill-blue-800" />
+      About
+      <!--<UserIcon className="w-7 h-7 fill-txt" />-->
     </button>
     <button class="cursor-pointer" on:click={() => page.set("jobs")}>
-      <BriefcaseIcon className="w-6 h-6 fill-blue-800" />
+      Experience
+      <!--<BriefcaseIcon className="w-7 h-7 fill-txt" />-->
     </button>
     <button class="cursor-pointer" on:click={() => page.set("projects")}>
-      <BulbIcon className="w-6 h-6 fill-blue-800" />
+      Projects
+      <!--<BulbIcon className="w-7 h-7 fill-txt" />-->
     </button>
   </div>
 </nav>
