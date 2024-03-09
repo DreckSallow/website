840a66efee128cd39ffbcf3f894d7de4a9ae9507
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-08 23:41:37 -0500

    feat: add project card component

---
+++ src/components/cards/project.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Tag from "../ui/tag.svelte";
+
+  export let title: string;
+  export let img: string | ImageMetadata;
+  export let imgAlt: string;
+  export let desc: string;
+  export let tags: string[] = [];
+</script>
+
+<article>
+  <img
+    loading="lazy"
+    decoding="asnyc"
+    src={img}
+    alt={imgAlt}
+    class="rounded-2xl h-[200px] w-full"
+    height="200px"
+  />
+  <div class="p-2">
+    <h4 class="font-semibold text-xl mb-2">{title}</h4>
+    <p class="text-base">{desc}</p>
+    <ul class="mt-4">
+      {#each tags as tag}
+        <Tag text={tag} className="mr-2" />
+      {/each}
+    </ul>
+  </div>
+</article>
