a16b3ec938b3e6138031e905638defc787f3652b
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-08 23:41:25 -0500

    feat: add tag component

---
+++ src/components/ui/tag.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let text: String;
+  export let className: string = "";
+</script>
+
+<span
+  class={"rounded-full bg-brand/30 px-3 py-1 text-xs inline-block font-normal " +
+    className}
+>
+  {text}
+</span>
