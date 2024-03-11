9f35dfa157043ec6fba1ae090d384c750c7951fa
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-10 22:02:09 -0500

    feat: add social icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,3 +1,8 @@
 export { default as UserIcon } from "./user.svelte"
 export { default as BulbIcon } from "./bulb.svelte"
 export { default as BriefcaseIcon } from "./briefcase.svelte"
+
+
+export { default as GithubIcon } from "./github.svelte"
+export { default as LinkedinIcon } from "./linkedin.svelte"
+export { default as MailIcon } from "./mail.svelte"
