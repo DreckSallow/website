fc1ed130a1c403dd2f74650c00a357b6bc2717a9
Dreck Sallow <dikson1.aranda.cabrera@gmail.com>
2024-03-15 11:58:26 -0500

    refactor: remove unused code

--- src/components/sections.svelte
+++ src/components/sections.svelte
@@ -4,30 +4,26 @@
   import ProjectsSection from "./views/projects.svelte";
   import { page } from "../store";
 
-  let applyClass = false;
-  $: currentPage = $page;
-  //TODO: block keybinding when is on transition
-  page.subscribe((pg) => {
-    if (currentPage == $page) return;
-    /*applyClass = true;
-    setTimeout(() => {
-      currentPage = pg;
-    }, 1600);
-    setTimeout(() => {
-      applyClass = false;
-    }, 2600);*/
-  });
+  // $: currentPage = $page;
+  // //TODO: block keybinding when is on transition
+  // page.subscribe(() => {
+  //   if (currentPage == $page) return;
+  //   /*applyClass = true;
+  //   setTimeout(() => {
+  //     currentPage = pg;
+  //   }, 1600);
+  //   setTimeout(() => {
+  //     applyClass = false;
+  //   }, 2600);*/
+  // });
 </script>
 
-{#if currentPage == "info"}
+{#if $page == "info"}
   <AboutSection />
-{:else if currentPage == "jobs"}
+{:else if $page == "jobs"}
   <JobsSection />
-{:else if currentPage == "projects"}
+{:else if $page == "projects"}
   <ProjectsSection />
 {/if}
 
-<div
-  class="fixed bottom-0 left-0 w-screen bg-txt z-40"
-  class:animate-pager={applyClass}
-/>
+<div class="fixed bottom-0 left-0 w-screen bg-txt z-40" />
